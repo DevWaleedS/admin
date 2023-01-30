10d94f010eb3ac95d9ef322b8349ae71a08f4394
DevWaleedS <eng.waleed.salah@outlook.com>
2023-01-31 01:14:58 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import ContextProvider from "./store/ContextProvider";
 import NotificationProvider from "./store/NotificationProvider";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
